df5ad2af5b228b5eaccc238fe7c235e255f66183
Naveen <sheoran19997@gmail.com>
2023-09-05 12:41:07 +0530

    add nested route

---
+++ pages/aboutus/developer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function developer() {
+  return (
+    <div>developer</div>
+  )
+}
+
+export default developer
---
+++ pages/aboutus/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function aboutus() {
+  return (
+    // <div>aboutus</div>
+    <h1>aboutus</h1>
+  )
+}
+
+export default aboutus;
